8b2168f95c59861bbf8542100da63a1d92fffc5e
Matt Mielke <matt.mielke@digikey.com>
2019-09-05 12:35:34 -0500

    I2C working on NUCLEO-L476RG
    At this point, I am able to use the I2C bus to request a $GPGGA sentence
    and parse the recieved sentence. Note that the teseo module must first
    be configured as an I2C Positioning Sensor (AN5203).

--- files/common/headers/teseo_liv3f.h
+++ files/common/headers/teseo_liv3f.h
@@ -1,45 +1,54 @@
 #ifndef ATMO_TESEO_LIV3F_H_
 #define ATMO_TESEO_LIV3F_H_
 
+/******************************************************************************/
+/*                                  Includes                                  */
+/******************************************************************************/
+//#include "../gpio/gpio.h"
 
-#include "../atmo/core.h"
-#include "../app_src/atmosphere_platform.h"
 
+typedef enum {
+    ATMO_TeseoLIV3F_Status_Success              = 0x00u,  // Common - Operation was successful
+    ATMO_TeseoLIV3F_Status_Fail                 = 0x01u,  // Common - Operation failed
+    ATMO_TeseoLIV3F_Status_Initialized          = 0x02u,  // Common - Peripheral already initialized
+    ATMO_TeseoLIV3F_Status_Invalid              = 0x03u,  // Common - Invalid operation or result
+    ATMO_TeseoLIV3F_Status_NotSupported         = 0x04u,  // Common - Feature not supported by platform
+} ATMO_TeseoLIV3F_Status_t;
 
 typedef struct 
 {
-  ATMO_GPIO_Device_Pin_t powerPin;
-  ATMO_GPIO_Device_Pin_t wupPin;
-  ATMO_GPIO_Device_Pin_t rstPin;
-  ATMO_DriverInstanceHandle_t gpioDriverInstance;
-  ATMO_DriverInstanceHandle_t i2cDriverInstance;
+    ATMO_GPIO_Device_Pin_t wupPin;
+    ATMO_GPIO_Device_Pin_t rstPin;
+    ATMO_DriverInstanceHandle_t gpioDriverInstance;
+    ATMO_DriverInstanceHandle_t i2cDriverInstance;
 } ATMO_TeseoLIV3F_Config_t;
 
 typedef struct
 {
-  uint32_t hrsUtc;
-  uint32_t minutesUtc;
-  uint32_t secondsUtc;
-  float latitude;
-  float longitude;
-  bool posFix;
-  uint32_t num_satellites;
-  float altitude;
-}ATMO_TeseoLIV3F_LocData_t;
-
-typedef enum
-{
-  ATMO_TeseoLIV3F_Status_Success = 0,
-  ATMO_TeseoLIV3F_Status_Fail
-} ATMO_TeseoLIV3F_Status_t;
+	uint32_t hrsUtc;
+	uint32_t minutesUtc;
+	uint32_t secondsUtc;
+	float latitude;
+	float longitude;
+	float altitude;
+	ATMO_BOOL_t posFix;
+	uint32_t numSatellites;
+} ATMO_TeseoLIV3F_LocData_t;
 
 
-ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_Init( ATMO_TeseoLIV3F_Config_t *config );
-float ATMO_TeseoLIV3F_GetLatitude();
-float ATMO_TeseoLIV3F_GetLongitude();
-bool ATMO_TeseoLIV3F_PosFix();
-uint32_t ATMO_TeseoLIV3F_GetNumSatellites();
-float ATMO_TeseoLIV3F_GetAltitude();
+
+/******************************************************************************/
+/*                         Public Function Prototypes                         */
+/******************************************************************************/
+ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_Init(ATMO_TeseoLIV3F_Config_t *config);
+ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_SetConfiguration(const ATMO_TeseoLIV3F_Config_t *config);
+ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_GetLocation( void );
+ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_GetLatitude( void );
+ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_GetLongitude( void );
+ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_PosFix( void );
+ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_GetNumSatellites( void );
+ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_GetAltitude( void );
+
 
 #endif
 
--- files/common/objects/teseo_liv3f.c
+++ files/common/objects/teseo_liv3f.c
@@ -1,9 +1,37 @@
+/******************************************************************************/
+/*                                  Includes                                  */
+/******************************************************************************/
+#include "../app_src/atmosphere_platform.h"
 #include "teseo_liv3f.h"
 
-#define BUFF_SIZE 100
+
+/******************************************************************************/
+/*                                  Defines                                   */
+/******************************************************************************/
+#define GPGLL_MSG_LEN 22
+#define I2C_RX_BUFF_LEN 101
 
 
-// global variables
+typedef struct {
+    ATMO_TeseoLIV3F_Config_t config;
+    unsigned int locRxAbilityHandle;
+    unsigned int latRxAbilityHandle;
+    unsigned int longRxAbilityHandle;
+    ATMO_BOOL_t latRxAbilityHandleSet;
+    ATMO_BOOL_t longRxAbilityHandleSet;
+    ATMO_BOOL_t locRxAbilityHandleSet;
+    ATMO_BOOL_t configured;
+} ATMO_TeseoLIV3F_Priv_Config;
+
+
+/******************************************************************************/
+/*                              Global Variables                              */
+/******************************************************************************/
+static ATMO_TeseoLIV3F_Priv_Config _ATMO_TeseoLIV3F_config;
+
+static ATMO_BOOL_t lastLocDataReceived = false;
+static ATMO_TeseoLIV3F_LocData_t lastLocData;
+/*
 // Private internal storage of driver configuration
 static ATMO_TeseoLIV3F_Config_t _ATMO_TeseoLIV3F_PrivConfig;
 static ATMO_TeseoLIV3F_LocData_t last_location_data;
@@ -12,62 +40,334 @@ volatile int collecting_identifier = 0;
 volatile int test = 0;
 char rx_buff[BUFF_SIZE] = {0};
 int rx_buff_ind = 0;
+*/
+
+static const uint16_t teseo_slave_addr = 0x3A;
+static const char *gpgga_msg = "$PSTMNMEAREQUEST,2,0\r\n";
+
+char rx_buff[I2C_RX_BUFF_LEN + 1];
 
 
-// Private function prototypes
-void uart5_custom_init( void );
-void parse_gpgga_sentence( char *str );
+/******************************************************************************/
+/*                        Private Function Prototypes                         */
+/******************************************************************************/
+ATMO_BOOL_t ATMO_TeseoLIV3F_ParseMessage( char* str, ATMO_TeseoLIV3F_LocData_t *msg );
 
 
-void UART5_IRQHandler( void )
+/******************************************************************************/
+/*                        Private Function Definitions                        */
+/******************************************************************************/
+/*static void ATMO_TeseoLIV3F_Pulse()
 {
-  char rx_char;
-
-  if ( READ_BIT( UART5->ISR, USART_ISR_RXNE ) ) // Data was received
-  {
-    // if rx '$'
-    // then collect data until ','
-    // if data = GPGGA
-    // then collect UTC, lat, lon, fix?, num sats, altitude
-    // ALWAYS check for '$'
-
-    rx_char = READ_REG( UART5->RDR );
-    if ( rx_char == '$' )
+    if(!_ATMO_TeseoLIV3F_config.configured)
     {
-      if ( collect_nmea_sentence != 0 )
-      {
-        parse_gpgga_sentence( rx_buff );
-      }
-      collect_nmea_sentence = 1;
-      collecting_identifier = 1;
-      rx_buff_ind = 0;
-      return;
+        return;
     }
-    else if ( collect_nmea_sentence != 0 )
-    {
-      if ( ( rx_char == ',' ) && ( collecting_identifier != 0 ) )
-      {
-        collecting_identifier = 0;
-        rx_buff[rx_buff_ind] = '\0'; // add null terminator
-        if ( strncmp( rx_buff, "GPGGA", BUFF_SIZE ) )
-        {
-          collect_nmea_sentence = 0;
-          return;
-        }
-        rx_buff_ind = 0; // discard identifier
-      }
-      else
-      {
-        rx_buff[rx_buff_ind] = rx_char;
-        rx_buff_ind++;
-      }
-    }
-  }
+	ATMO_GPIO_SetPinState(_ATMO_TeseoLIV3F_config.config.gpioDriverInstance, _ATMO_TeseoLIV3F_config.config.powerPin, ATMO_GPIO_PinState_Low);
+	ATMO_PLATFORM_DelayMilliseconds(200);
+	ATMO_GPIO_SetPinState(_ATMO_TeseoLIV3F_config.config.gpioDriverInstance, _ATMO_TeseoLIV3F_config.config.powerPin, ATMO_GPIO_PinState_High);
+	ATMO_PLATFORM_DelayMilliseconds(3000);
+	ATMO_GPIO_SetPinState(_ATMO_TeseoLIV3F_config.config.gpioDriverInstance, _ATMO_TeseoLIV3F_config.config.powerPin, ATMO_GPIO_PinState_Low);
+	ATMO_PLATFORM_DelayMilliseconds(200);
 }
 
 
-// Function definitions
+void ATMO_TeseoLIV3F_FactoryReset()
+{
+    if(!_ATMO_TeseoLIV3F_config.configured)
+    {
+        return;
+    }
+	uint32_t dummy = 0;
+	ATMO_UART_WriteBlocking(_ATMO_TeseoLIV3F_config.config.uartDriverInstance, "$PSRF101,0,0,0,000,0,0,12,8*1C\r\n", strlen("$PSRF101,0,0,0,000,0,0,12,8*1C\r\n"), &dummy, 100);
+}
 
+
+void ATMO_TeseoLIV3F_DisableOutput()
+{
+    if(!_ATMO_TeseoLIV3F_config.configured)
+    {
+        return;
+    }
+	uint32_t dummy = 0;
+	ATMO_UART_WriteBlocking(_ATMO_TeseoLIV3F_config.config.uartDriverInstance, "$PSRF103,02,00,00,01*26\r\n", strlen("$PSRF103,02,00,00,01*26\r\n"), &dummy, 100);
+	ATMO_UART_WriteBlocking(_ATMO_TeseoLIV3F_config.config.uartDriverInstance, "$PSRF103,03,00,00,01*27\r\n", strlen("$PSRF103,03,00,00,01*27\r\n"), &dummy, 100);
+	ATMO_UART_WriteBlocking(_ATMO_TeseoLIV3F_config.config.uartDriverInstance, "$PSRF103,04,00,00,01*20\r\n", strlen("$PSRF103,04,00,00,01*20\r\n"), &dummy, 100);
+	ATMO_UART_WriteBlocking(_ATMO_TeseoLIV3F_config.config.uartDriverInstance, "$PSRF103,00,00,00,01*24\r\n", strlen("$PSRF103,00,00,00,01*24\r\n"), &dummy, 100);
+}
+
+
+uint8_t ATMO_TeseoLIV3F_NMEAChecksum(const char *str)
+{
+	while(*str == '$')
+	{
+		str++;
+	}
+
+	uint8_t checksum = 0;
+
+	while(*str != '*')
+	{
+		checksum ^= *str;
+		str++;
+	}
+
+	return checksum;
+}
+
+
+void ATMO_TeseoLIV3F_SetReportRate(int reportRateHz)
+{
+	uint32_t dummy = 0;
+	char cmdStr[128];
+	sprintf(cmdStr, "$PSRF103,00,00,%d,01*", reportRateHz);
+	uint8_t checksum = ATMO_TeseoLIV3F_NMEAChecksum(cmdStr);
+	sprintf(cmdStr + strlen(cmdStr), "%02X\r\n", checksum);
+	ATMO_UART_WriteBlocking(_ATMO_TeseoLIV3F_config.config.uartDriverInstance, cmdStr, strlen(cmdStr), &dummy, 100);
+}
+*/
+
+
+void ATMO_TeseoLIV3F_RxCb(void *data)
+{
+	char str[256];
+	ATMO_GetString((ATMO_Value_t *)data, str, 256);
+
+	ATMO_PLATFORM_DebugPrint("Str: %s\r\n", str);
+
+    // Try to get location info
+    ATMO_TeseoLIV3F_LocData_t locData;
+    if(ATMO_TeseoLIV3F_ParseMessage(str, &locData))
+    {
+        lastLocDataReceived = true;
+        memcpy(&lastLocData, &locData, sizeof(locData));
+
+        if(_ATMO_TeseoLIV3F_config.locRxAbilityHandleSet)
+        {
+            ATMO_Value_t val;
+            ATMO_CreateValueBinary(&val, &locData, sizeof(locData));
+            ATMO_AddAbilityExecute(_ATMO_TeseoLIV3F_config.locRxAbilityHandle, &val);
+        }
+        if(_ATMO_TeseoLIV3F_config.longRxAbilityHandleSet)
+        {
+            ATMO_Value_t val;
+            ATMO_CreateValueFloat(&val, locData.longitude);
+            ATMO_AddAbilityExecute(_ATMO_TeseoLIV3F_config.longRxAbilityHandle, &val);
+        }
+        if(_ATMO_TeseoLIV3F_config.latRxAbilityHandleSet)
+        {
+            ATMO_Value_t val;
+            ATMO_CreateValueFloat(&val, locData.latitude);
+            ATMO_AddAbilityExecute(_ATMO_TeseoLIV3F_config.latRxAbilityHandle, &val);
+        }
+    }
+}
+
+
+
+ATMO_BOOL_t ATMO_TeseoLIV3F_ParseMessage( char* str, ATMO_TeseoLIV3F_LocData_t *msg )
+{
+	const char delim[] = "\xFF" ",";
+	char *token;
+
+	// Should be $GPGGA
+	token = strtok( str, delim );
+	if( token == NULL || strlen( token ) < 6 || strncmp( token, "$GPGGA", 6 ) )
+	{
+		return false;
+	}
+
+	// UTC of position
+	token = strtok( NULL, delim );
+	if ( token == NULL || strlen( token ) < 6 )
+	{
+		return false;
+	}
+	msg->hrsUtc = ( token[0] & 0xF ) * 10 + ( token[1] & 0xF );
+	msg->minutesUtc = ( token[2] & 0xF ) * 10 + ( token[3] & 0xF );
+	msg->secondsUtc = ( token[4] & 0xF ) * 10 + ( token[5] & 0xF );
+
+	// Latitude of position
+	token = strtok( NULL, delim );
+	if ( token == NULL || strlen( token ) < 4 )
+	{
+		return false;
+	}
+	msg->latitude = ( token[0] & 0xF ) * 10 + ( token[1] & 0xF ) + ( strtof( token + 2, NULL ) / 60.0 );
+
+	// N or S
+	token = strtok( NULL, delim );
+	if ( token == NULL || strlen( token ) < 1 )
+	{
+		return false;
+	}
+	if ( *token == 'S' )
+	{
+		msg->latitude *= -1;
+	}
+
+	// Longitude of position
+	token = strtok( NULL, delim );
+	if ( token == NULL || strlen( token ) < 4 )
+	{
+		return false;
+	}
+	msg->longitude = ( token[0] & 0xF ) * 100 + ( token[1] & 0xF ) * 10 + ( token[2] & 0xF ) + ( strtof( token + 3, NULL ) / 60.0 );
+
+	// E or W
+	token = strtok( NULL, delim );
+	if ( token == NULL || strlen( token ) < 1 )
+	{
+		return false;
+	}
+	if ( *token == 'W' )
+	{
+		msg->longitude *= -1;
+	}
+
+	// GPS Quality indicator
+	token = strtok( NULL, delim );
+	if ( token == NULL || strlen( token ) < 1 )
+	{
+		return false;
+	}
+	if ( *token == '0' )
+	{
+		msg->posFix = false;
+	}
+	else
+	{
+		msg->posFix = true;
+	}
+
+	// number of satellites in use
+	token = strtok( NULL, delim );
+	if ( token == NULL || strlen( token ) < 2 )
+	{
+		return false;
+	}
+	msg->numSatellites = ( token[0] & 0xF ) * 10 + ( token[1] & 0xF );
+
+	// horizontal dilution of precision (throw away)
+	token = strtok( NULL, delim );
+	if ( token == NULL )
+	{
+		return false;
+	}
+
+	// Antenna altitude above mean-sea-level
+	token = strtok( NULL, delim );
+	if ( token == NULL )
+	{
+		return false;
+	}
+	msg->altitude = strtof( token, NULL );
+
+	// M
+
+	return true;
+}
+
+
+/******************************************************************************/
+/*                        Public Function Definitions                         */
+/******************************************************************************/
+ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_Init(ATMO_TeseoLIV3F_Config_t *config)
+{
+	memset(&_ATMO_TeseoLIV3F_config, 0, sizeof(_ATMO_TeseoLIV3F_config));
+
+    // Did the user supply a configuration?
+    if( config )
+    {
+        ATMO_TeseoLIV3F_SetConfiguration(config);
+    }
+    else
+    {
+    	_ATMO_TeseoLIV3F_config.configured = false;
+    }
+
+    return ATMO_TeseoLIV3F_Status_Success;
+}
+
+
+ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_SetConfiguration(const ATMO_TeseoLIV3F_Config_t *config)
+{
+    if( config == NULL )
+    {
+        return ATMO_TeseoLIV3F_Status_Fail;
+    }
+
+    memcpy( &_ATMO_TeseoLIV3F_config.config, config, sizeof(ATMO_TeseoLIV3F_Config_t) );
+    _ATMO_TeseoLIV3F_config.configured = true;
+
+    // Initialize I2C
+   // ATMO_I2C_Init(config->i2cDriverInstance);
+
+    //ATMO_I2C_Peripheral_t i2cConfig;
+	//i2cConfig.operatingMode = ATMO_I2C_OperatingMode_Master;
+	//i2cConfig.baudRate = ATMO_I2C_BaudRate_Standard_Mode;
+	//ATMO_I2C_SetConfiguration(config->i2cDriverInstance, &i2cConfig);
+
+    // Reset GPS
+	ATMO_GPIO_Config_t gpioConfig;
+	gpioConfig.initialState = ATMO_GPIO_PinState_Low;
+	gpioConfig.pinMode = ATMO_GPIO_PinMode_Output_PushPull;
+	ATMO_GPIO_SetPinConfiguration(config->gpioDriverInstance, config->rstPin, &gpioConfig);
+
+	// At least 100ms
+	ATMO_PLATFORM_DelayMilliseconds(300);
+	ATMO_GPIO_SetPinState(config->gpioDriverInstance, config->rstPin, ATMO_GPIO_PinState_High);
+
+	//gpioConfig.initialState = ATMO_GPIO_PinState_Low;
+	//ATMO_GPIO_SetPinConfiguration(config->gpioDriverInstance, config->powerPin, &gpioConfig);
+
+	gpioConfig.initialState = ATMO_GPIO_PinState_High;
+	ATMO_GPIO_SetPinConfiguration(config->gpioDriverInstance, config->wupPin, &gpioConfig);
+/*
+    // Reset GPS
+    ATMO_TeseoLIV3F_Pulse();
+	ATMO_PLATFORM_DelayMilliseconds(100);
+
+	while(ATMO_GPIO_Read(config->gpioDriverInstance, config->wupPin) != ATMO_GPIO_PinState_High)
+	{
+		ATMO_TeseoLIV3F_Pulse();
+	}
+
+    ATMO_TeseoLIV3F_DisableOutput();
+    ATMO_TeseoLIV3F_SetReportRate(config->reportFreq);
+
+    ATMO_PLATFORM_DebugPrint("GPS Initialized\r\n");*/
+
+    return ATMO_TeseoLIV3F_Status_Success;
+}
+
+
+ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_GetLocation( void )
+{
+	ATMO_TeseoLIV3F_LocData_t locData;
+
+	ATMO_I2C_MasterWrite( _ATMO_TeseoLIV3F_config.config.i2cDriverInstance, teseo_slave_addr, NULL, 0, (uint8_t*)gpgga_msg, GPGLL_MSG_LEN, 0xFFFFFFFF );
+
+	ATMO_PLATFORM_DelayMilliseconds( 50 );
+
+	ATMO_I2C_MasterRead( _ATMO_TeseoLIV3F_config.config.i2cDriverInstance, teseo_slave_addr,  NULL, 0, (uint8_t*)rx_buff, I2C_RX_BUFF_LEN, 0xFFFFFFFF );
+
+	ATMO_TeseoLIV3F_ParseMessage( rx_buff, &locData );
+
+	memcpy( &lastLocData, &locData, sizeof(locData) );
+
+/*
+    if(!lastLocDataReceived)
+    {
+        return ATMO_TeseoLIV3F_Status_Fail;
+    }
+
+    memcpy(locData, &lastLocData, sizeof(lastLocData));
+*/
+    return ATMO_TeseoLIV3F_Status_Success;
+}
+
+/*
 ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_Init( ATMO_TeseoLIV3F_Config_t *config )
 {
   if ( config == NULL )
@@ -82,171 +382,39 @@ ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_Init( ATMO_TeseoLIV3F_Config_t *config 
 
   return ATMO_TeseoLIV3F_Status_Success;
 }
+*/
 
-
-float ATMO_TeseoLIV3F_GetLatitude()
+ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_GetLatitude( void )
 {
-  return last_location_data.latitude;
+	return lastLocData.latitude + lastLocData.longitude + lastLocData.altitude;
+  //return last_location_data.latitude;
 }
 
 
-float ATMO_TeseoLIV3F_GetLongitude()
+ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_GetLongitude( void )
 {
-  return last_location_data.longitude;
+	return lastLocData.longitude;
+  //return last_location_data.longitude;
 }
 
 
-bool ATMO_TeseoLIV3F_PosFix()
+ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_PosFix( void )
 {
-  return last_location_data.posFix;
+  //return last_location_data.posFix;
 }
 
 
-uint32_t ATMO_TeseoLIV3F_GetNumSatellites()
+ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_GetNumSatellites( void )
 {
-  return last_location_data.num_satellites;
+  //return last_location_data.num_satellites;
 }
 
 
-float ATMO_TeseoLIV3F_GetAltitude()
+ATMO_TeseoLIV3F_Status_t ATMO_TeseoLIV3F_GetAltitude( void )
 {
-  return last_location_data.altitude;
+	return lastLocData.altitude;
+  //return last_location_data.altitude;
 }
 
 
-void uart5_custom_init( void )
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  /* UART5 clock enable */
-  __HAL_RCC_UART5_CLK_ENABLE();
-
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  /**UART5 GPIO Configuration
-  PD2     ------> UART5_RX
-  PC12     ------> UART5_TX
-  */
-  GPIO_InitStruct.Pin = GPIO_PIN_2;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF8_UART5;
-  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
-
-  GPIO_InitStruct.Pin = GPIO_PIN_12;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF8_UART5;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-  
-  /* configure uart5 */
-  CLEAR_BIT( UART5->CR1, USART_CR1_UE ); // disable uart
-  WRITE_REG( UART5->BRR, ( 80e6 / 9600.0 ) + 0.5 ); // round
-  SET_BIT( UART5->CR1, USART_CR1_TE | USART_CR1_RE ); // enable TX and RX
-
-  /* Enable Interrupts */
-  SET_BIT( UART5->CR1, USART_CR1_RXNEIE ); // read data register not empty
-  NVIC_EnableIRQ( UART5_IRQn ); // enable all USART1 interrupts globally
-  NVIC_SetPriority( UART5_IRQn, 0 );    // highest priority
-
-  /* Enable USART1 */
-  SET_BIT( UART5->CR1, USART_CR1_UE );
-}
-
-
-void parse_gpgga_sentence( char *str )
-{
-  const char delim[2] = ",";
-  char *token;
-
-
-  // UTC of position
-  token = strtok( str, delim );
-  if ( token == NULL || strlen( token ) < 6 )
-  {
-    return;
-  }
-  last_location_data.hrsUtc = ( token[0] & 0xF ) * 10 + ( token[1] & 0xF );
-  last_location_data.minutesUtc = ( token[2] & 0xF ) * 10 + ( token[3] & 0xF );
-  last_location_data.secondsUtc = ( token[4] & 0xF ) * 10 + ( token[5] & 0xF );
-
-  // Latitude of position
-  token = strtok( NULL, delim );
-  if ( token == NULL || strlen( token ) < 4 )
-  {
-    return;
-  }
-  last_location_data.latitude = ( token[0] & 0xF ) * 10 + ( token[1] & 0xF ) + ( strtof( token + 2, NULL ) / 60.0 );
-
-  // N or S
-  token = strtok( NULL, delim );
-  if ( token == NULL || strlen( token ) < 1 )
-  {
-    return;
-  }
-  if ( *token == 'S' )
-  {
-    last_location_data.latitude *= -1;
-  }
-
-  // Longitude of position
-  token = strtok( NULL, delim );
-  if ( token == NULL || strlen( token ) < 4 )
-  {
-    return;
-  }
-  last_location_data.longitude = ( token[0] & 0xF ) * 100 + ( token[1] & 0xF ) * 10 + ( token[2] & 0xF ) + ( strtof( token + 3, NULL ) / 60.0 );
-
-  // E or W
-  token = strtok( NULL, delim );
-  if ( token == NULL || strlen( token ) < 1 )
-  {
-    return;
-  }
-  if ( *token == 'W' )
-  {
-    last_location_data.longitude *= -1;
-  }
-
-  // GPS Quality indicator
-  token = strtok( NULL, delim );
-  if ( token == NULL || strlen( token ) < 1 )
-  {
-    return;
-  }
-  if ( *token == '0' )
-  {
-    last_location_data.posFix = false;
-  }
-  else
-  {
-    last_location_data.posFix = true;
-  }
-
-  // number of satellites in use
-  token = strtok( NULL, delim );
-  if ( token == NULL || strlen( token ) < 2 )
-  {
-    return;
-  }
-  last_location_data.num_satellites = ( token[0] & 0xF ) * 10 + ( token[1] & 0xF );
-
-  // horizontal dilution of precision
-  token = strtok( NULL, delim );
-  if ( token == NULL )
-  {
-    return;
-  }
-
-  // Antenna altitude above mean-sea-level
-  token = strtok( NULL, delim );
-  if ( token == NULL )
-  {
-    return;
-  }
-  last_location_data.altitude = strtof( token, NULL );
-
-  // M
-}
